8096bb6b67726cdbb9c545453f9d884eb07383c7
Benjamin Milde <benni@kobrakai.de>
2015-01-08 18:46:40 +0100

    Added contest template

---
+++ contests.php
@@ -0,0 +1,25 @@
+<?php 
+
+$projects = [];
+
+foreach($page->find("template=contest, sort=sort") as $p){
+	$projects[] = array(
+		'title' => $p->title,
+		'url' => $p->url,
+		'image' => array(
+			'url' => $p->project_images->first()->size(375, 250)->url,
+			'desc' => $p->project_images->description
+		),
+		'meta_type' => array(
+			'title' => "Wettbewerb"
+		),
+		'meta_status' => $p->meta_status->title
+	);
+}
+
+$templateData = array(
+	'title' => $page->title,
+	'types' => $page->children->explode("title"),
+	'status' => $pages->find("parent_id=1027")->explode("title"),
+	'projects' => $projects
+);
