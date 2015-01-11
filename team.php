6b0a4bcfe995f551aa1756c5be9423ed755050a3
Benjamin Milde <benni@kobrakai.de>
2015-01-11 11:26:32 +0100

    first setup team template

---
+++ team.php
@@ -0,0 +1,33 @@
+<?
+$fields = array("management", "office", "architects", "constructionists", "interns");
+
+$templateData = array(
+	'title' => $page->title,
+	'teams' => array(
+		array(
+			'title' => $page->fields->get("management")->label,
+			'name' => 'management',
+			'items' => $page->management->getArray()
+		),
+		array(
+			'title' => $page->fields->get("office")->label,
+			'name' => 'office',
+			'items' => $page->office->getArray()
+		),
+		array(
+			'title' => $page->fields->get("architects")->label,
+			'name' => 'architects',
+			'items' => $page->architects->getArray()
+		),
+		array(
+			'title' => $page->fields->get("constructionists")->label,
+			'name' => 'constructionists',
+			'items' => $page->constructionists->getArray()
+		),
+		array(
+			'title' => $page->fields->get("interns")->label,
+			'name' => 'interns',
+			'items' => $page->interns->getArray()
+		)
+	)
+);
